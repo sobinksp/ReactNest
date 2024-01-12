f4d7b7994b1e5ba90a05054975753d581ba5abc2
sobin <Sobingeneral@gmail.com>
2024-01-12 21:01:59 +0700

    feat: add new routes (history/create)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,17 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./components/Home";
+// import Home from "./components/Home";
+import CreateInspection from "./components/CreateInspection";
+import History from "./components/History";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<History />} />
         {/* <Route exact path="/" element={}></Route> */}
+        <Route path="/create" element={<CreateInspection />} />
       </Routes>
     </BrowserRouter>
   );
