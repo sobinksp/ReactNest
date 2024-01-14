dc03ff9a9e7b320aa447173ee7109cd9e2aeb201
sobin <Sobingeneral@gmail.com>
2024-01-14 18:57:19 +0700

    feat: update useState and make ui more responsive

--- frontend/src/components/CreateInspection.tsx
+++ frontend/src/components/CreateInspection.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, useEffect, useState } from "react";
+import { ChangeEvent, FormEvent, useEffect, useState } from "react";
 import Header from "./Header";
 import { useNavigate } from "react-router-dom";
 import { BASE_API_URL } from "../ApiConfig";
@@ -8,7 +8,15 @@ interface Standard {
   id: string;
   name: string;
   createDate: string;
-  standardData: string[];
+  standardData: Array<{
+    conditionMax: string;
+    conditionMin: string;
+    key: string;
+    name: string;
+    shape: string[];
+    maxLength: number;
+    minLength: number;
+  }>;
 }
 
 const CreateInspection = () => {
@@ -18,18 +26,30 @@ const CreateInspection = () => {
   //     }
   //   `;
   const navigate = useNavigate();
-  const [price, setPrice] = useState("");
+  const [name, setName] = useState("");
+
   const [standards, setStandards] = useState<Standard[]>([]);
+  const [selectedStandard, setSelectedStandard] = useState<Standard | null>(
+    null
+  );
+
+  const [file, setFile] = useState<File | null>(null);
+  const [note, setNote] = useState("");
+  const [price, setPrice] = useState<string | null>(null);
+  const [sampling, setSampling] = useState<string[]>([]);
+  const [datetime, setDatetime] = useState<string | null>(null);
 
   const fetchStandards = async () => {
     try {
       const res = await fetch(`${BASE_API_URL}/standard`);
+      // const res = await fetch("https://easyrice-es-trade-data.s3.ap-southeast-1.amazonaws.com/standards.json")
       const data = await res.json();
       setStandards(data);
     } catch (error) {
       console.log(error);
     }
   };
+
   useEffect(() => {
     fetchStandards();
   }, []);
@@ -39,26 +59,93 @@ const CreateInspection = () => {
       String(input)
     );
   };
+  // name
+  const handleName = (e: ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
+  // standard selection
+  const handleSelect = (e: ChangeEvent<HTMLSelectElement>) => {
+    const standardId = e.target.value;
+    const selected = standards.find((s) => s.id === standardId);
+    if (selected) {
+      setSelectedStandard(selected);
+    }
+    // setSelectedStandard(e.target.value as Standard | null);
+  };
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+  // file upload
+  const handleFile = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) setFile(e.target.files[0]);
+  };
+  const handleNote = (e: ChangeEvent<HTMLInputElement>) => {
+    setNote(e.target.value);
+  };
+
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
     const { id, value } = e.target;
+    // upload file
+    if (id === "jsonFile") {
+      setFile(e.target.files[0]);
+    }
 
+    // regex for price input
     if (id === "price" && isValidPrice(Number(value))) {
       setPrice(value);
     }
   };
-
+  const handlePrice = (e: ChangeEvent<HTMLInputElement>) => {
+    setPrice(e.target.value);
+  };
+  const handleSampling = (e: ChangeEvent<HTMLInputElement>) => {
+    setSampling((prevSampling) =>
+      e.target.checked
+        ? [...prevSampling, e.target.value]
+        : prevSampling.filter((item) => item !== e.target.value)
+    );
+  };
+  const handleDatetime = (e: ChangeEvent<HTMLInputElement>) => {
+    setDatetime(e.target.value);
+  };
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const res = await fetch(`${BASE_API_URL}/history`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "Application/json",
+        },
+        body: JSON.stringify({
+          name: name,
+          standardID: selectedStandard?.id,
+          note: note,
+          standardName: selectedStandard?.name,
+          samplingDate: datetime,
+          samplingPoint: sampling,
+          price: price,
+          standardData: selectedStandard?.standardData,
+        }),
+      });
+      if (res.ok) {
+        const data = await res.json();
+        console.log(data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   const handleCancelClick = () => {
     navigate("/");
   };
 
   return (
     <Header>
-      <h1 className="text-center mb-4">{"Create  Inspection"}</h1>
+      <h1 className="text-center mb-4 ">{"Create  Inspection"}</h1>
       <div className="d-flex justify-content-center ">
-        <div className="card shadow border-white" style={{ width: "60vh" }}>
+        <div className="card shadow border-white" style={{ maxWidth: "370px" }}>
           <div className="card-body">
-            <form>
+            <form onSubmit={handleSubmit}>
               <div className="d-flex flex-column mb-2">
                 <label className="fw-semibold form-label">Name*</label>
                 <input
@@ -66,19 +153,24 @@ const CreateInspection = () => {
                   id="name"
                   placeholder="Inspection Name"
                   required
-                  onChange={handleChange}
+                  onChange={handleName}
                   className="form-control"
                 />
               </div>
               <div className="d-flex flex-column mb-2">
-                <label className="fw-semibold form-label">Name*</label>
-                <select className="form-select" required>
+                <label className="fw-semibold form-label">Standard*</label>
+                <select
+                  className="form-select"
+                  required
+                  id="standard"
+                  onChange={handleSelect}
+                >
                   {/* <option value="" disabled={true}> */}
-                  <option style={{ backgroundColor: "green" }} hidden value="">
+                  <option hidden value="">
                     Please Select Standard
                   </option>
                   {standards.map((standard) => (
-                    <option key={standard.id} value={standard.name}>
+                    <option key={standard.id} value={standard.id}>
                       {standard.name}
                     </option>
                   ))}
@@ -97,10 +189,10 @@ const CreateInspection = () => {
                 <label className="fw-semibold form-label">Upload File</label>
                 <input
                   type="file"
+                  accept=".json"
                   id="jsonFile"
                   placeholder="Inspection Name"
-                  required
-                  onChange={handleChange}
+                  onChange={handleFile}
                   className="form-control"
                 />
               </div>
@@ -111,7 +203,7 @@ const CreateInspection = () => {
                   type="text"
                   id="note"
                   placeholder="Note"
-                  onChange={handleChange}
+                  onChange={handleNote}
                   className="form-control"
                 />
               </div>
@@ -121,8 +213,8 @@ const CreateInspection = () => {
                   type="number"
                   id="price"
                   placeholder="10,000"
-                  value={price}
-                  onChange={handleChange}
+                  value={price || ""}
+                  onChange={handlePrice}
                   className="form-control"
                 />
               </div>
@@ -133,6 +225,8 @@ const CreateInspection = () => {
                     <input
                       className="form-check-input border-dark"
                       type="checkbox"
+                      value="Front End"
+                      onChange={handleSampling}
                     />
                     <label className="form-check-label">Front End</label>
                   </div>
@@ -140,6 +234,8 @@ const CreateInspection = () => {
                     <input
                       className="form-check-input border-dark"
                       type="checkbox"
+                      value="Back End"
+                      onChange={handleSampling}
                     />
                     <label className="form-check-label">Back End</label>
                   </div>
@@ -147,6 +243,8 @@ const CreateInspection = () => {
                     <input
                       className="form-check-input border-dark"
                       type="checkbox"
+                      value="Other"
+                      onChange={handleSampling}
                     />
                     <label className="form-check-label">Other</label>
                   </div>
@@ -160,6 +258,7 @@ const CreateInspection = () => {
                   className="form-control text-muted"
                   type="datetime-local"
                   id="datetime"
+                  onChange={handleDatetime}
                 />
               </div>
               <div className="d-flex justify-content-end">
@@ -170,7 +269,10 @@ const CreateInspection = () => {
                 >
                   Cancel
                 </button>
-                <button className="btn text-white button-color ml-3">
+                <button
+                  type="submit"
+                  className="btn text-white button-color ml-3"
+                >
                   Submit
                 </button>
               </div>
